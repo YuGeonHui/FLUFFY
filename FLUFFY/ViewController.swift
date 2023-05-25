11684481c73f40d71bd705e6ec1e1fd84b1291e3
YuGeonHui <ghyu_0906@naver.com>
2023-05-25 13:12:14 +0900

    애플 로그인을 위한 기본셋팅

--- FLUFFY/ViewController.swift
+++ FLUFFY/ViewController.swift
@@ -6,14 +6,14 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
-
-
 }
 
