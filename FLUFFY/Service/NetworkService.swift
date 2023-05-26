975a8cf53d4e69ea9d98d2424c55571d2cabaa02
keem-hyun <hw813@naver.com>
2023-05-26 19:26:18 +0900

    통신 메서드 수정

--- FLUFFY/Service/NetworkService.swift
+++ FLUFFY/Service/NetworkService.swift
@@ -12,67 +12,64 @@ struct NetworkService {
     
     
     // MARK: - GET
-    func getRequest() {
-        let url = "https://jsonplaceholder.typicode.com/todos/1"
-        
-        let header : HTTPHeaders = ["Content-Type":"application/json", "Accept":"application/json"]
+    func getRequest<T: Decodable>(url: String, parameters: [String: Any]?, headers: [String: String]?, completion: @escaping (Result<T, Error>) -> Void) {
         
         AF.request(
             url, // [주소]
             method: .get, // [전송 타입]
-            parameters: nil, // [전송 데이터]
+            parameters: parameters, // [전송 데이터]
             encoding: JSONEncoding.default, // [인코딩 스타일]
-            headers: header // [헤더 지정]
-        )
-        .validate(statusCode: 200..<300)
-        .responseData { response in
-            print(response)
-        }
-    }
-    
-    // MARK: - Post
-    func postRequest() {
-        let url = ""
-        
-        AF.request(
-            url, // [주소]
-            method: .post, // [전송 타입]
-            parameters: nil, // [전송 데이터]
-            encoding: JSONEncoding.default, // [인코딩 스타일]
-            headers: nil // [헤더 지정]
+            headers: headers // [헤더 지정]
         )
         .validate(statusCode: 200..<300)
         .responseData { response in
             switch response.result {
-            case .success(let res):
-                print("응답 코드 :: \(response.response?.statusCode ?? 0)")
-                print("응답 데이터 :: \(String(data: res, encoding: .utf8) ?? "")")
-            case .failure(let err):
-                print("응답 코드 :: \(response.response?.statusCode ?? 0)")
-                print("에 러 :: \(err.localizedDescription)")
-                break
+            case .success(let value):
+                completion(.success(value))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    // MARK: - Post
+    func postRequest<T: Decodable>(url: String, parameters: [String: Any]?, headers: [String: String]?, completion: @escaping (Result<T, Error>) -> Void) {
+        
+        AF.request(
+            url, // [주소]
+            method: .post, // [전송 타입]
+            parameters: parameters, // [전송 데이터]
+            encoding: JSONEncoding.default, // [인코딩 스타일]
+            headers: headers // [헤더 지정]
+        )
+        .validate(statusCode: 200..<300)
+        .responseData { response in
+            switch response.result {
+            case .success(let value):
+                completion(.success(value))
+            case .failure(let error):
+                completion(.failure(error))
             }
         }
     }
     
     // MARK: - Delete
-    func deleteRequest() {
-        let url = ""
+    func deleteRequest<T: Decodable>(url: String, parameters: [String: Any]?, headers: [String: String]?, completion: @escaping (Result<T, Error>) -> Void) {
         
         AF.request(
             url,
             method: .delete,
-            parameters: nil,
+            parameters: parameters,
             encoding: JSONEncoding.default,
-            headers: nil
+            headers: headers
         )
         .validate(statusCode: 200..<300)
         .responseData { response in
             switch response.result {
-            case .success(let res):
-                print(res)
+            case .success(let value):
+                completion(.success(value))
             case .failure(let error):
-                print(error)
+                completion(.failure(error))
             }
         }
     }
