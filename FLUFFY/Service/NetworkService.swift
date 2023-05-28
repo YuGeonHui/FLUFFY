6f312c4d62eeac7c404124fa29b5e80c00a96d95
keem-hyun <hw813@naver.com>
2023-05-29 01:30:02 +0900

    통신 메서드 통합

--- FLUFFY/Service/NetworkService.swift
+++ FLUFFY/Service/NetworkService.swift
@@ -10,78 +10,18 @@ import Alamofire
 
 struct NetworkService {
     
-    
-    // MARK: - GET
-    func getRequest<T: Decodable>(url: String, parameters: [String: Any]?, headers: HTTPHeaders?, completion: @escaping (Result<T, Error>) -> Void) {
+    // MARK: - Perform Request
+    func performRequest<T: Decodable, U: Encodable>(url: String, method: HTTPMethod, parameters: U?, headers: HTTPHeaders?, completion: @escaping (Result<T, Error>) -> Void) {
         
         AF.request(
             url,
-            method: .get,
-            parameters: parameters,
-            encoding: JSONEncoding.default,
-            headers: headers        )
-        .validate(statusCode: 200..<300)
-        .responseDecodable(of: T.self) { response in
-            switch response.result {
-            case .success(let value):
-                completion(.success(value))
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
-    }
-    
-    // MARK: - Post
-    func postRequest<T: Decodable>(url: String, parameters: [String: Any]?, headers: HTTPHeaders?, completion: @escaping (Result<T, Error>) -> Void) {
-        
-        AF.request(
-            url,
-            method: .post,
-            parameters: parameters,
-            encoding: JSONEncoding.default,
-            headers: headers
-        )
-        .validate(statusCode: 200..<300)
-        .responseDecodable(of: T.self) { response in
-            switch response.result {
-            case .success(let value):
-                completion(.success(value))
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
-    }
-    
-    // MARK: - Delete
-    func deleteRequest<T: Decodable>(url: String, completion: @escaping (Result<T, Error>) -> Void) {
-        
-        AF.request(
-            url,
-            method: .delete
-        )
-        .validate(statusCode: 200..<300)
-        .responseDecodable(of: T.self) { response in
-            switch response.result {
-            case .success(let value):
-                completion(.success(value))
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
-    }
-    
-    // MARK: - Update
-    func updateRequest<T: Encodable, U: Decodable>(url: String, parameters: T?, headers: HTTPHeaders?, completion: @escaping (Result<U, Error>) -> Void) {
-        
-        AF.request(
-            url,
-            method: .put,
+            method: method,
             parameters: parameters as? Parameters,
             encoding: JSONEncoding.default,
             headers: headers
         )
         .validate(statusCode: 200..<300)
-        .responseDecodable(of: U.self) { response in
+        .responseDecodable(of: T.self) { response in
             switch response.result {
             case .success(let value):
                 completion(.success(value))
@@ -90,9 +30,8 @@ struct NetworkService {
             }
         }
     }
-
-    
 }
+
 
 
 
