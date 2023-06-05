2cf89cc89375259b724b59e86b0670ef42715fb6
keem-hyun <hw813@naver.com>
2023-06-05 22:49:36 +0900

    수정 모달 뷰 구현

---
+++ FLUFFY/Scence/Scheduler/EditModalViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EditModalViewController.swift
+//  FLUFFY
+//
+//  Created by 김강현 on 2023/06/05.
+//
+
+import UIKit
+
+class EditModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
